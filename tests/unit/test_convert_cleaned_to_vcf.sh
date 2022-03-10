b34b9f27c29bf1ad36abc220d19ad6e84178bf2d
pappewaio <jesper.gadin@gmail.com>
2022-03-10 12:09:17 +0100

    fixes the issue with metadata and also adds the possibility to forward kgp allele frequencies to vcf

--- tests/unit/test_convert_cleaned_to_vcf.sh
+++ tests/unit/test_convert_cleaned_to_vcf.sh
@@ -31,8 +31,9 @@ function _check_results {
 }
 
 function _run_script {
+base="${1}"
 
-  "${test_script}.sh" input.tsv.gz observed-result1.tsv.gz false
+  "${test_script}.sh" input.tsv.gz observed-result1.tsv.gz false ${base}
 
   _check_results <(zcat observed-result1.tsv.gz | grep -v "##fileDate=") <(zcat expected-result1.tsv.gz | grep -v "##fileDate=")
 
@@ -71,16 +72,17 @@ cat <<EOF | gzip -c > ./expected-result1.tsv.gz
 ##FORMAT=<ID=SE,Number=A,Type=Float,Description="Standard error of effect size estimate">
 ##FORMAT=<ID=EZ,Number=A,Type=Float,Description="Z-score of effect size estimate">
 ##FORMAT=<ID=EP,Number=A,Type=Float,Description="p-value for effect estimate">
+##FORMAT=<ID=AFKG,Number=A,Type=Float,Description="Reference allele frequency from 1000 genomes project. Population used is defined in the metadata of the summary stats">
 #CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	sumstats
-10	10574522	rs2025468	T	C	.	PASS	.	ES:SE:EZ:EP	0.0101:0.0165:0.612121:0.5398
-10	102814179	rs284858	T	C	.	PASS	.	ES:SE:EZ:EP	0.0187:0.0132:1.41667:0.1592
-10	106371703	rs1409409	C	A	.	PASS	.	ES:SE:EZ:EP	-0.005:0.0171:-0.292398:0.7713
-10	107148593	rs12781860	A	C	.	PASS	.	ES:SE:EZ:EP	0.0016:0.0241:0.06639:0.9482
-10	113128849	rs1362943	G	A	.	PASS	.	ES:SE:EZ:EP	-0.018:0.0136:-1.32353:0.187
-10	118368257	rs12767500	C	T	.	PASS	.	ES:SE:EZ:EP	-0.052:0.0308:-1.68831:0.09108
+10	10574522	rs2025468	T	C	.	PASS	.	ES:SE:EZ:EP:AFKG	0.0101:0.0165:0.612121:0.5398:0.82
+10	102814179	rs284858	T	C	.	PASS	.	ES:SE:EZ:EP:AFKG	0.0187:0.0132:1.41667:0.1592:0.41
+10	106371703	rs1409409	C	A	.	PASS	.	ES:SE:EZ:EP:AFKG	-0.005:0.0171:-0.292398:0.7713:1
+10	107148593	rs12781860	A	C	.	PASS	.	ES:SE:EZ:EP:AFKG	0.0016:0.0241:0.06639:0.9482:0.92
+10	113128849	rs1362943	G	A	.	PASS	.	ES:SE:EZ:EP:AFKG	-0.018:0.0136:-1.32353:0.187:1
+10	118368257	rs12767500	C	T	.	PASS	.	ES:SE:EZ:EP:AFKG	-0.052:0.0308:-1.68831:0.09108:1
 EOF
 
-_run_script "CHR"
+_run_script "Reference"
 
 #---------------------------------------------------------------------------------
 # Next case
