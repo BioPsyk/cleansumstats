2acb8eab43f9c3481b86ba9adcf9b6d43cb1e2f0
pappewaio <jesper.gadin@gmail.com>
2025-02-06 13:43:04 +0100

    second attempt to fix nextflow internet issue

--- cleansumstats.sh
+++ cleansumstats.sh
@@ -349,6 +349,10 @@ FAKE_HOME="${outdir_container}"
 export SINGULARITY_HOME="${FAKE_HOME}"
 export APPTAINER_HOME="${FAKE_HOME}"
 
+# Set Nextflow environment variables for the launching environment
+export NXF_OFFLINE="true"
+
+
 # Previous fake home, causing #FAKE_HOME="tmp/fake-home"
 #export SINGULARITY_HOME="/cleansumstats/${FAKE_HOME}"
 #mkdir -p "${FAKE_HOME}"
@@ -426,22 +430,22 @@ elif [ "${runtype}" == "test" ] || [ "${runtype}" == "utest" ] || [ "${runtype}"
   if [ "${container_image}" == "dockerhub_biopsyk" ]; then
     echo "container: $runimage"
     mount_flags=$(format_mount_flags "-v")
-    #exec docker run --rm "${deploy_image_tag_docker_hub}" "${run_script}"
-    exec docker run --rm ${mount_flags} "${runimage}" ${run_script}
+    exec docker run --rm ${mount_flags} -e NXF_OFFLINE="${NXF_OFFLINE}" "${runimage}" ${run_script}
   elif [ "${container_image}" == "docker" ]; then
     echo "container: $runimage"
     mount_flags=$(format_mount_flags "-v")
-    exec docker run --rm ${mount_flags} "${runimage}" ${run_script}
+    exec docker run --rm ${mount_flags} -e NXF_OFFLINE="${NXF_OFFLINE}" "${runimage}" ${run_script}
   else
     echo "container: $runimage"
     mount_flags=$(format_mount_flags "-B")
-    singularity run --contain --cleanenv ${mount_flags} "${runimage}" ${run_script}
+    SINGULARITYENV_NXF_OFFLINE="${NXF_OFFLINE}" singularity run --contain --cleanenv ${mount_flags} "${runimage}" ${run_script}
   fi
 elif [ "${container_image}" == "dockerhub_biopsyk" ]; then
   echo "container: $runimage"
   mount_flags=$(format_mount_flags "-v")
   exec docker run \
      --rm \
+     -e NXF_OFFLINE="${NXF_OFFLINE}" \
      ${mount_flags} \
      -v "${indir_host}:${indir_container}" \
      -v "${outdir_host}:${outdir_container}" \
@@ -464,6 +468,7 @@ elif [ "${container_image}" == "docker" ]; then
   mount_flags=$(format_mount_flags "-v")
   exec docker run \
      --rm \
+     -e NXF_OFFLINE="${NXF_OFFLINE}" \
      ${mount_flags} \
      -v "${indir_host}:${indir_container}" \
      -v "${outdir_host}:${outdir_container}" \
@@ -485,36 +490,27 @@ else
   echo "container: $runimage"
   mount_flags=$(format_mount_flags "-B")
   
-  # Set Nextflow environment variables in the launching environment
-  export NXF_HOME="/cleansumstats/.nextflow"
-  export NXF_OFFLINE="true"
-  
-  if [ "${container_image}" == "docker" ]; then
-    # For Docker, pass environment variables using -e flags
-    singularity run \
-       --contain \
-       --cleanenv \
-       -e NXF_HOME="${NXF_HOME}" \
-       -e NXF_OFFLINE="${NXF_OFFLINE}" \
-       ${mount_flags} \
-       ${extrapaths2} \
-       -B "${indir_host}:${indir_container}" \
-       -B "${outdir_host}:${outdir_container}" \
-       -B "${dbsnpdir_host}:${dbsnpdir_container}" \
-       -B "${kgpdir_host}:${kgpdir_container}" \
-       -B "${tmpdir_host}:${tmpdir_container}" \
-       -B "${workdir_host}:${workdir_container}" \
-       "${runimage}" \
-       nextflow \
-         -log "${outdir_container}/.nextflow.log" \
-         run ${run_script} \
-         --extrapaths ${extrapaths3} \
-         ${devmode} \
-         --input "${infile_container}" \
-         --outdir "${outdir_container}" \
-         --libdirdbsnp "${dbsnpdir_container}" \
-         --kg1000AFGRCh38 "${kgpfile_container}"
-  fi
+  SINGULARITYENV_NXF_OFFLINE="${NXF_OFFLINE}" singularity run \
+     --contain \
+     --cleanenv \
+     ${mount_flags} \
+     ${extrapaths2} \
+     -B "${indir_host}:${indir_container}" \
+     -B "${outdir_host}:${outdir_container}" \
+     -B "${dbsnpdir_host}:${dbsnpdir_container}" \
+     -B "${kgpdir_host}:${kgpdir_container}" \
+     -B "${tmpdir_host}:${tmpdir_container}" \
+     -B "${workdir_host}:${workdir_container}" \
+     "${runimage}" \
+     nextflow \
+       -log "${outdir_container}/.nextflow.log" \
+       run ${run_script} \
+       --extrapaths ${extrapaths3} \
+       ${devmode} \
+       --input "${infile_container}" \
+       --outdir "${outdir_container}" \
+       --libdirdbsnp "${dbsnpdir_container}" \
+       --kg1000AFGRCh38 "${kgpfile_container}"
 fi
 
 if ${pathquicktest}; then
